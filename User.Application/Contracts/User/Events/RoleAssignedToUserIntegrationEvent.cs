faa5b073628e622a35fc0e9398f5b9ce188203d0
alstelmach <73331704+alstelmach@users.noreply.github.com>
2021-04-20 18:07:11 +0200

    Moved integration events to application layer (#2)
    * Created project structure
    
    * Started implementing user registration
    
    * Created user registration functionality
    
    * Updated appsettings
    
    * Implemented validators unit tests and made few little improves
    
    * Implemented authentication
    
    * Implemented get user endpoint
    
    * Implemented jwt validation
    
    * Implemented change password functionality
    
    * Added password change tests
    
    * Implemented user deletion
    
    * Started implementing role and permission
    
    * Created role permission assignment functionality
    
    * Assigned role to user
    
    * Implemented application layer for role manipulation
    
    * Created authorization functionality
    
    * Fixed some bugs
    
    * Moved ascore dependencies
    
    * Fixed a bug and use higher infrastructure AsCore version
    
    * Added docker support
    
    * Updated README.md
    
    * Moved integration events to application layer
    
    Co-authored-by: Aleksander Stelmach <astelmach@ess.etisoft.pl>

---
+++ User.Application/Contracts/User/Events/RoleAssignedToUserIntegrationEvent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using AsCore.Application.Abstractions.Messaging.Events;
+
+namespace User.Application.Contracts.User.Events
+{
+    public sealed class RoleAssignedToUserIntegrationEvent : IntegrationEvent
+    {
+        public RoleAssignedToUserIntegrationEvent(Guid userId,
+            Guid roleId)
+                : base(userId,
+                    nameof(RoleAssignedToUserIntegrationEvent)) =>
+                        RoleId = roleId;
+                        
+        public Guid RoleId { get; }
+    }
+}
---
+++ User.Application/Contracts/User/Events/UserDeletedIntegrationEvent.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using AsCore.Application.Abstractions.Messaging.Events;
+
+namespace User.Application.Contracts.User.Events
+{
+    public sealed class UserDeletedIntegrationEvent : IntegrationEvent
+    {
+        public UserDeletedIntegrationEvent(Guid userId)
+            : base(userId,
+                nameof(UserDeletedIntegrationEvent))
+        {
+        }
+    }
+}
